05b357eeb6852e1de7a266592f9e9aefe0c33c43
doctorrokter <doctorrokter@gmail.com>
2020-10-02 19:52:08 +0300

    - expose prop types

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,22 @@
-import Button from "./buttons/Button";
-import RaisedButton from "./buttons/RaisedButton";
-import RaisedTextButton from "./buttons/RaisedTextButton";
-import TextButton from "./buttons/TextButton";
+import Button, {ButtonDefaultProps, ButtonProps} from "./buttons/Button";
+import RaisedButton, {RaisedButtonProps, RaisedButtonDefaultProps} from "./buttons/RaisedButton";
+import RaisedTextButton, {RaisedTextButtonProps, RaisedTextButtonDefaultProps} from "./buttons/RaisedTextButton";
+import TextButton, {TextButtonProps, TextButtonDefaultProps} from "./buttons/TextButton";
 
 export {
     Button,
+    ButtonProps,
+    ButtonDefaultProps,
+
     RaisedTextButton,
+    RaisedTextButtonProps,
+    RaisedTextButtonDefaultProps,
+
     RaisedButton,
+    RaisedButtonProps,
+    RaisedButtonDefaultProps,
+
     TextButton,
+    TextButtonProps,
+    TextButtonDefaultProps
 }
